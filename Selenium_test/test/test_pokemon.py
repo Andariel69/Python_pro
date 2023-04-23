7da37648b85a1425b54b6f3a45d4a2a00b80b9a8
Elena <ill-elena-ciobanu@qa.studio>
2023-04-23 23:02:28 +0300

    Selenium_test

--- Selenium_test/test/test_pokemon.py
+++ Selenium_test/test/test_pokemon.py
@@ -1,11 +1,26 @@
 import requests
 import pytest
+import json
 
 def test_status_code():
     token = "19abc49a742406ed9cdeb924814d44fa"
-    
-response = requests.post('https://pokemonbattle.me:9104/pokemons' headers= trainer_token : {token}, json={
+    response = requests.post('https://pokemonbattle.me:9104/pokemons', headers={'trainer_token' : token}, json={
     "name": "Бумер",
     "photo": "https://dolnikov.ru/pokemons/albums/001.png"
 })
-assert response.status_code == 201
+    assert response.status_code == 400
+
+
+def test_part_of_body():
+    response = requests.get('https://pokemonbattle.me:9104/trainers', params={'trainer_id' : 4095})
+    response_body = response.text
+    js = json.loads(response_body)
+    assert js['trainer_name'] == 'Kim'
+
+
+@pytest.mark.parametrize('key, value', [('trainer_name', 'Kim'), ('city', 'Narita') ])
+
+def test_parts_of_body(key, value):
+    response = requests.get('https://pokemonbattle.me:9104/trainers', params={'trainer_id' : 4095})
+    # response_body = response.text
+    assert response.json()[key] == value
