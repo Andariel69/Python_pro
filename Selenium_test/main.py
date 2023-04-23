e7b4cf1f0b861cb6cc962025f3cef8878d70f3d3
Elena <ill-elena-ciobanu@qa.studio>
2023-04-23 23:26:13 +0300

    main.py

--- Selenium_test/main.py
+++ Selenium_test/main.py
@@ -1,6 +1,6 @@
 import requests
 
-
+#create_account
 response = requests.post('https://pokemonbattle.me:9104/trainers/reg', json={
     "trainer_token": "19abc49a742406ed9cdeb924814d44fa",
     "email": "443332@.ru",
@@ -9,28 +9,49 @@ response = requests.post('https://pokemonbattle.me:9104/trainers/reg', json={
 
 print (response.status_code)
 
-
+#variables
 token = "19abc49a742406ed9cdeb924814d44fa"
 URL = "https://pokemonbattle.me:9104/"
 num = f'мой токен это {token}'
 
 
-
+#trainer_activation_email
 response_activation = requests.post(f'{URL}trainers/confirm_email', json={
     "trainer_token": token
 })
 
 print (response_activation.text)
 
-response_add_pokemon = requests.post(f'{URL}pokemons', headers={'trainer_token' : token}, json={
+#create_pokemon
+response_create_pokemon = requests.post(f'{URL}pokemons', headers={'trainer_token' : token}, json={
     "name": "Булька",
     "photo": "https://dolnikov.ru/pokemons/albums/001.png"
 })
 
-print (response_add_pokemon.text)
+print (response_create_pokemon.text)
 
+#get_pokemons
 response = requests.get(f'{URL}pokemons', params={"trainer_id" : 4095})
-print (response_add_pokemon.text)
+print (response_create_pokemon.text)
 
+#add_pokemon_in_pokeball
+response_add_pokeball = requests.post(f'{URL}trainers/add_pokeball', headers={'trainer_token' : token}, json={
+    "pokemon_id": "9402"
+})
 
+print (response_add_pokeball.text)
+
+#change_name_pokemon
+response_change_name = requests.put(f'{URL}pokemons', headers={"trainer_token" : token}, json={
+    "pokemon_id": "9402",
+    "name": "Miu",
+    "photo": "https://dolnikov.ru/pokemons/albums/001.png"
+})
+
+print (response_change_name.text)
+
+#kill_pokemon
+response_kill_pokemon = requests.post(f'{URL}pokemons/kill', headers={"trainer_token" : token}, json={
+    "pokemon_id": "9404"
+})
 
