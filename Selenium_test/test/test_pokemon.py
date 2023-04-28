33f29d44f50fea6134a223d6e34750153eba4c9b
Andariel69 <126872848+Andariel69@users.noreply.github.com>
2023-04-28 13:25:20 +0300

    Update test_pokemon.py

--- Selenium_test/test/test_pokemon.py
+++ Selenium_test/test/test_pokemon.py
@@ -3,7 +3,7 @@ import pytest
 import json
 
 def test_status_code():
-    token = "19abc49a742406ed9cdeb924814d44fa"
+    token = 'AnyToken'
     response = requests.post('https://pokemonbattle.me:9104/pokemons', headers={'trainer_token' : token}, json={
     "name": "Бумер",
     "photo": "https://dolnikov.ru/pokemons/albums/001.png"
